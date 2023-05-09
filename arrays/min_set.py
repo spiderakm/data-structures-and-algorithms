fad2daada42b6cb0a6709ce388378927349f5da6
ashok kumar <spider.akm@gmail.com>
2023-05-10 00:08:07 +0530

    day4

--- arrays/min_set.py
+++ arrays/min_set.py
@@ -29,28 +29,6 @@ class Solution:
         return c
 
 
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-
-def main():
-
-    T = int(input())
-
-    while(T > 0):
-        n = int(input())
-        a = [int(x) for x in input().strip().split()]
-        ob=Solution()
-        print(ob.numofsubset(a, n))
-
-        T -= 1
-
-
-if __name__ == "__main__":
-    main()
-
-
 
 
 
